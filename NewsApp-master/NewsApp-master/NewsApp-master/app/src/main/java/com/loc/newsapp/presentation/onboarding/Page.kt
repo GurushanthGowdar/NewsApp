854c269b08a7ae059c1520b90412ba787c75c84b
GurushanthGowdar <guru22ca@gmail.com>
2024-02-18 14:15:45 +0530

    made the changes as required

--- NewsApp-master/NewsApp-master/NewsApp-master/app/src/main/java/com/loc/newsapp/presentation/onboarding/Page.kt
+++ NewsApp-master/NewsApp-master/NewsApp-master/app/src/main/java/com/loc/newsapp/presentation/onboarding/Page.kt
@@ -13,11 +13,11 @@ val pages = listOf(
     Page(
         title = "NewsPedia",
         description = "Stay Informed, Stay Ahead: Your Daily Dose of News, Anytime, Anywhere!",
-        image = R.drawable.onboarding1
+        image = R.drawable.newsscreen1
     ),
     Page(
         title = "Headlines in a Hurry: Your Quick News Fix!",
         description = "Empowering Your Day, One Headline at a Time: Unleash the Power of Knowledge with Our 24/7 News Hub!",
-        image = R.drawable.onboarding3
+        image = R.drawable.newsscreen2
     )
 )
